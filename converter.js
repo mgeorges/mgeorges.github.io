bafb2b8b9a7aacacead8d951b04a8d08da2935ff
mgeorges <mgeorges@users.noreply.github.com>
2017-05-28 16:22:19 -0400

    Update converter.js

--- converter.js
+++ converter.js
@@ -53,15 +53,121 @@ if (!String.prototype.repeat) {
 	}
 }
 
-/*Roman Numeral to Integer Conversion*/
+function Converter(fromType, toType, value) {
+	this.fromType = fromType;
+	this.toType = toType;
+	this.value = value;
+	this.positive = true;
+	this.note = '';
+	this.error = '';
+	this.convert = function() {
+		if (this['is_' + this.fromType]()){
+			if (this.fromType == this.toType){
+				return this.setSign(this.value);
+			}
+			else {
+				return this[this.fromType + '_to_' + this.toType]();
+			}
+		}
+		else {
+			return this.error;
+		}
+	}
+	this.setSign = function(result) {
+		if (this.positive) {
+			if (this.toType == 'binary'){
+				return '0-' + result;
+			}
+			return result;
+		}
+		else if (this.toType == 'integer') {
+			return -Math.abs(result);
+		}
+		else if (this.toType == 'binary') {
+			return '1-' + result;
+		}
+		else {
+			return '-' + result;
+		}
+	}
+}
 
-function roman_numeral_to_integer(number){
+Converter.prototype.is_integer = function() {
+	var num_val = Number(this.value);
+	if (num_val) {
+		//Set sign to positive or negative
+		if (num_val != Math.abs(num_val)) {
+			num_val = Math.abs(num_val);
+			this.positive = false;
+		}
+		if (num_val != Math.floor(num_val)){
+			if (this.positive) {
+				this.note = 'Number rounded down to nearest integer: ' + Math.floor(num_val).toString()+'. ';
+			}
+			else {
+				this.note = 'Number rounded up to nearest integer: ' + -Math.floor(num_val).toString()+'. ';
+			}
+		}
+		this.value = Math.floor(num_val);
+		return true;
+	}
+	else {
+		this.error = 'ERROR: ' + this.value + ' is not a valid input. Try Again';
+		return false;
+	}
+}
+
+Converter.prototype.is_roman_numeral = function() {
+	var roman_numeral_list = ['M', 'D', 'C', 'L', 'X', 'V', 'I'];
+
+	var temp_val = null;
+	if (this.value[0] == '-'){
+		this.positive = false;
+		temp_val = this.value.slice(1).toUpperCase();
+	}
+	else {
+		temp_val = this.value.toUpperCase();
+	}
+
+	for (var index = 0; index < temp_val.length; index++){
+		if (!(roman_numeral_list.contains(temp_val[index]))){
+			this.error = 'ERROR: '+ this.value + ' is not a valid set of roman numerals';
+			return false;
+		}
+	}
+	this.value = temp_val;
+	return true;
+}
+
+Converter.prototype.is_binary = function() {
+	var temp_val = this.value;
+	var io = ['1', '0'];
+	if (this.value.length >= 3){
+		if (this.value.slice(0, 2) == '1-') {
+			this.positive = false;
+			temp_val = this.value.slice(2);
+		}
+		else if (this.value.slice(0, 2) == '0-') {
+			 temp_val = this.value.slice(2);
+		}
+	}
+	for (var index = 0; index < temp_val.length; index++) {
+		if (!(io.contains(temp_val[index]))){
+			this.error = "ERROR: " + this.value + " is an invalid bit string. Try again.";
+			return false;
+		}
+	}
+	this.value = temp_val;
+	return true;
+}
+
+Converter.prototype.roman_numeral_to_integer = function(){
 	var conversion_dict = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1};
 	var md = ['M', 'D'];
 	var cl = ['C', 'L'];
 	var xv = ['X', 'V'];
 		
-	var roman_numeral = number.toUpperCase();
+	var roman_numeral = this.value.toUpperCase();
 	var result_int = 0;
 	var previous = null;
 	var last_added = 0;
@@ -84,28 +190,21 @@ function roman_numeral_to_integer(number){
 			return "ERROR: " + roman_numeral + " is not a valid set of roman numerals";
 		}
 	}
-	return result_int;
+	if (this.positive) {
+		return result_int;
+	}
+	else {
+		return -Math.abs(result_int);
+	}
 }
 
-function RomanToIntConversion(){
-	$("button#rnToInt-btn").on('click', function(){
-		var roman_numeral = document.getElementById("rnToInt-input").value;
-		var output = document.getElementById("rnToInt-output");
-		output.innerHTML = roman_numeral_to_integer(roman_numeral);
-	});
-}
-
-$("button#rnToInt-btn").on('click', RomanToIntConversion());
-
-/*Integer to Roman Numeral Conversion*/
-
-function integer_to_roman_numeral(number){
+Converter.prototype.integer_to_roman_numeral = function(){
 	var num_conversion_dict = {1000: 'M', 500: 'D', 100: 'C', 50: 'L', 10: 'X', 5: 'V', 1: 'I'};
-	var int_num = number;
+	var int_num = Number(this.value);
 	var powers = [1000, 100, 10, 1];
 	var result_rn = '';
-	if (number > 3999) {
-		return "ERROR: " + number + " is too large to be converted to Roman Numerals. Must be less than 3999. Try again";
+	if (this.value > 3999) {
+		return "ERROR: " + this.value + " is too large to be converted to Roman Numerals. Must be less than 3999. Try again";
 	}
 
 
@@ -136,27 +235,19 @@ function integer_to_roman_numeral(number){
 					result_rn += num_conversion_dict[p].repeat(div_num);
 				}
 			}
-			int_num = int_num % p
+			int_num = int_num % p;
 		}
 	}
-	return result_rn;
+	if (this.positive) {
+		return result_rn;
+	}
+	else {
+		return '-' + result_rn;
+	}
 }
-
-function IntToRomanConversion(){
-	$("button#intToRn-btn").on('click', function(){
-		var inty = document.getElementById("intToRn-input").value;
-		var output = document.getElementById("intToRn-output");
-		output.innerHTML = integer_to_roman_numeral(inty);
-	});
-}
-
-$("button#intToRn-btn").on('click', IntToRomanConversion());
-
-/*Binary to Integer Conversion*/
-
-function binary_to_integer(bits) {
-	var bit_string = bits;
-	var io = new Array('1', '0');
+	
+Converter.prototype.binary_to_integer = function() {
+	var bit_string = this.value;
 	var bit_pos = bit_string.length;
 	var result_int = 0;
 	
@@ -170,28 +261,22 @@ function binary_to_integer(bits) {
 			continue;
 		}
 		else {
-			return "ERROR: " + bits + " is an invalid bit string. Try again.";
+			return "ERROR: " + this.value + " is an invalid bit string. Try again.";
 		}
 	}
-	return result_int;
+	if (this.positive){
+		return result_int;
+	}
+	else {
+		return -Math.abs(result_int);
+	}
 }
-
-function BinaryToIntConversion() {
-	$("button#binToInt-btn").on('click', function(){
-		var binary = document.getElementById("binToInt-input").value;
-		var output = document.getElementById("binToInt-output");
-		output.innerHTML = binary_to_integer(binary);
-	});
-}
-
-$("button#binToInt-btn").on('click', BinaryToIntConversion());
-
-/*Integer to Binary Conversion*/
-
-function integer_to_binary(int) {
+	
+Converter.prototype.integer_to_binary = function() {
 	var result_string = '';
-	var comp_int = Math.abs(int);
-	if (int == 0){
+	var comp_int = this.value;
+	console.log(this.value);
+	if (this.value == 0){
 		return '0';
 	}
 
@@ -200,22 +285,65 @@ function integer_to_binary(int) {
 		result_string = bit.toString() + result_string;
 		comp_int = Math.floor(comp_int / 2);
 	}
-	if (int < 0) {
-		result_string = '1-' + result_string;
-	}
-	else {
+
+	if (this.positive) {
 		result_string = '0-' + result_string;
 	}
+	else {
+		result_string = '1-' + result_string;
+	}
+	this.note += 'The preceding bit indicates 1 if negative and 0 if positive.\n';
 	return result_string;
 }
 
-function IntToBinaryConversion() {
-	$("button#intToBin-btn").on('click', function(){
-		var int = document.getElementById("intToBin-input").value;
-		var output = document.getElementById("intToBin-output");
-		output.innerHTML = integer_to_binary(int);
-	});
+Converter.prototype.binary_to_roman_numeral = function() {
+	this.value = this.binary_to_integer();
+	if (this.is_integer()){
+		return this.integer_to_roman_numeral();
+	}
+	else {
+		this.error = "ERROR: Something went wrong"
+		return this.error;
+	}
 }
 
-$("button#intToBin-btn").on('click', IntToBinaryConversion());
+Converter.prototype.roman_numeral_to_binary = function() {
+	this.value = this.roman_numeral_to_integer();
+	if (this.is_integer()){
+		return this.integer_to_binary();
+	}
+	else {
+		this.error = "ERROR: Something went wrong"
+		return this.error;
+	}
+}
 
+Converter.prototype.html_display = function(result) {
+	var display_html = "";
+	if (this.note) {
+		display_html += "<p class='note'>" + this.note + "</p>";
+	}
+	if (this.error){
+		display_html += "<p class='answer center error'>" + result + "</p>";
+	}
+	else {
+		display_html += "<p class='answer center'>" + result + "</p>";
+	}
+	return display_html;
+}
+
+
+
+$(document).ready(function(){
+
+	$("button#convert").on('click', function(){
+		var from_type = document.getElementById('fromType').value;
+		var to_type = document.getElementById('toType').value;
+		var val = document.getElementById('value').value;
+		var output = document.getElementById('converter-output');
+
+		var conv = new Converter(from_type, to_type, val);
+		var result = conv.convert();
+		output.innerHTML = conv.html_display(result);
+	});
+});
